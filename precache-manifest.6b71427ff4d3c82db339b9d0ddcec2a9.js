f149a18019c4c8588499b6d4b587c0e8f564670d
설영환 <yhsul@seol-yeonghwan-ui-MacBookPro.local>
2020-09-04 20:03:37 +0900

    build data

---
+++ precache-manifest.6b71427ff4d3c82db339b9d0ddcec2a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfa934baeab3f2a74ee152646ba6c5b",
+    "url": "/new_portfolio/index.html"
+  },
+  {
+    "revision": "046809bbea74bbb0662b",
+    "url": "/new_portfolio/static/css/main.6e0e62f1.chunk.css"
+  },
+  {
+    "revision": "fe11ad60717c16fae467",
+    "url": "/new_portfolio/static/js/2.716dcadf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new_portfolio/static/js/2.716dcadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046809bbea74bbb0662b",
+    "url": "/new_portfolio/static/js/main.750ee753.chunk.js"
+  },
+  {
+    "revision": "b7d223b1e140b57ab8ac",
+    "url": "/new_portfolio/static/js/runtime-main.a70873af.js"
+  },
+  {
+    "revision": "3eb240bd713d52a12a3d5c705187eefb",
+    "url": "/new_portfolio/static/media/encryption.3eb240bd.jpg"
+  },
+  {
+    "revision": "e4d5be5032c19e45234841fd41adbb0d",
+    "url": "/new_portfolio/static/media/keypad.e4d5be50.jpg"
+  },
+  {
+    "revision": "b9630ccfdc244defd720b27354248409",
+    "url": "/new_portfolio/static/media/responsibleweb.b9630ccf.jpg"
+  },
+  {
+    "revision": "cc11904c45dca3dcf7da4460356a119c",
+    "url": "/new_portfolio/static/media/standard1.cc11904c.png"
+  }
+]);
